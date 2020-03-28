0ec3812fb9a0e27e89741b9923b03f38525d3781
User <notfound@email.com>
2020-03-28 18:46:25 +1000

    Without navigation bar

--- android/src/rf/gd/theoneboringmancompany/growham/AndroidLauncher.java
+++ android/src/rf/gd/theoneboringmancompany/growham/AndroidLauncher.java
@@ -1,6 +1,7 @@
 package rf.gd.theoneboringmancompany.growham;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -14,6 +15,8 @@ public class AndroidLauncher extends AndroidApplication {
 		config.useGyroscope = false;
 		config.useCompass = false;
 		config.useAccelerometer = false;
+		this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+		this.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 		initialize(new Main(), config);
 	}
 }
